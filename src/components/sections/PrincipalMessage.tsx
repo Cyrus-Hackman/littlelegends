7cebec4c6accc4a471614ddacd25350adf7b3dd1
Cyrus-Hackman <cyrusapenteng11@gmail.com>
2026-03-02 08:13:12 +0000

    change principal image

--- src/components/sections/PrincipalMessage.tsx
+++ src/components/sections/PrincipalMessage.tsx
@@ -18,7 +18,7 @@ export function PrincipalMessage() {
                   transition={{ duration: 0.3 }}
                 >
                   <ParallaxImage 
-                    src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=600&h=800&fit=crop"
+                    src="https://ik.imagekit.io/cyrus/image.png"
                     alt="Dr. Joycelyn Poku - Principal"
                     speed={0.2}
                   />
